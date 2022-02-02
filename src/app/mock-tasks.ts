b6c54a9c4e7c7c0efe88b7671320905209cbbad2
Belen <santurtunbelen@gmail.com>
2022-02-02 20:40:21 -0300

    Servicio y Servidor de tareas: Listo

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,30 +1,6 @@
 import { Task } from "./Task" // importa la interface
 
 export const TASKS : Task [] = [
-    {
-        id: 1,
-        text: "Terminar primer modulo de Angular",
-        day: "Febrero 3, a las 12:00 hs",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Hacer las compras para la cena",
-        day: "Febrero 3, a las 17:00 hs",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Investigas sobre Bootstrap",
-        day: "Febrero 3, a las 19:00 hs",
-        reminder: false,
-    },
-    {
-        id: 4,
-        text: "Leer un libro",
-        day: "Febrero 3, a las 21:00 hs",
-        reminder: false,
-    },
-
+    
 
 ]
